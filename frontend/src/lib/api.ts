c37d011dc2d661373a513e12955e322066789f89
Jahanara Khatun <zarakhatun58@gmail.com>
2026-07-30 00:36:07 +0530

    update

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,12 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://investment-referral.onrender.com/api",
 });
-console.log(
-  "API URL:",
-  import.meta.env.VITE_API_URL
-);
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
